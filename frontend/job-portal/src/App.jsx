cff88e080daca2f0382012807f73ea52f2fdaaef
Tu204845 <tu975482@gmail.com>
2026-05-13 18:27:30 +0700

    Fix: Login import case sensitivity for Linux/Vercel

--- frontend/job-portal/src/App.jsx
+++ frontend/job-portal/src/App.jsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignUp from "./pages/Auth/SignUp";
-import Login from "./pages/Auth/login";
+import Login from "./pages/Auth/Login";
 
 import JobSeekerDashboard from "./pages/JobSeeker/JobSeekerDashboard";
 import JobDetails from "./pages/JobSeeker/JobDetails";
